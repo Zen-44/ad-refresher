ecd8e1fc3f2b49dc9473407f4c69198478c2c129
Zen <92331357+Zen-44@users.noreply.github.com>
2024-01-13 15:21:27 +0200

    fix log file path

--- utils/log.py
+++ utils/log.py
@@ -1,7 +1,11 @@
 from datetime import datetime
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "..", "output.log")
 
 def log(string):
     current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f'{current_datetime}: {string}')
-    with open("output.log", "a+") as file:
+    with open(file_path, "a+") as file:
         print(f'{current_datetime}: {string}', file = file)
