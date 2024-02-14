882252c0f1304c542b243a85822c7aa60ea41724
Zen <92331357+Zen-44@users.noreply.github.com>
2024-02-14 12:50:01 +0200

    update licenses

--- utils/ads.py
+++ utils/ads.py
@@ -1,3 +1,9 @@
+'''
+Copyright (c) 2020 Idena
+Distributed under the MIT software license, see the accompanying   
+file COPYING or http://www.opensource.org/licenses/mit-license.php.
+'''
+
 import utils.proto_file as proto_file
 from binascii import unhexlify
 from io import BytesIO
